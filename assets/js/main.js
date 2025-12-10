8247f8292911879f1de20b330970364e3f8fe2a0
Santhosh9789 <93291820+Santhosh9789@users.noreply.github.com>
2025-12-10 09:26:22 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,7 +233,16 @@
    * Init Lenis Smooth Scroll
    */
   function initLenis() {
-    const lenis = new Lenis();
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4x87
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
 
     function raf(time) {
       lenis.raf(time);
