7b470059c29f8f80e4e7a7413e4c30c6466a1b6f
Santhosh9789 <93291820+Santhosh9789@users.noreply.github.com>
2025-12-20 12:47:18 +0530

    Add files via upload

--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,7 @@
 @echo off
-echo Starting Local Server with Clean URL Support...
+echo Starting Localhost Server...
+echo Visit: http://localhost:8000
+echo (Opening browser automatically...)
+start http://localhost:8000
 python server.py
 pause
